b36382229a5cac02d6b0925192b0c1cae8efaa77
Shannja <shannuel22m@gmail.com>
2026-03-19 17:40:29 -0600

    coolio presence

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -1,20 +1,33 @@
 import os from 'os';
-import { version } from 'discord.js';
+import { ActivityType, Client, Collection, version } from 'discord.js';
 import { botLog, LogType } from '../utils/logger';
 import { PlayFabAdmin } from 'playfab-sdk';
-const { titleId, devSecret } = require('../settings.json');
+const { titleId, devSecret, guildId } = require('../settings.json');
+
+export interface FuckassClient extends Client {
+    commands: Collection<string, any>;
+    events: Collection<string, any>;
+    startupTime: number;
+} // because typescript wont shutup about types
 
 export const name = 'clientReady';
 export const once = true;
 
-export function execute(client: any) {
+export async function execute(client: FuckassClient) {
     PlayFabAdmin.settings.titleId = titleId;
     PlayFabAdmin.settings.developerSecretKey = devSecret;
+
+    const guild = await client.guilds.fetch(guildId);
+
+    client.user!.setPresence({
+        activities: [{ name: guild.name, type: ActivityType.Watching }],
+        status: 'dnd'
+    });
 
     const uptime = (Date.now() - client.startupTime) / 1000;
     const commandCount = client.commands.size;
     const eventCount = client.events.size;
-    
+
     botLog("\n-# ---------------------------------", LogType.INFO); 
     botLog(`Startup completed in ${uptime}s.`, LogType.INFO);
     botLog(`Bot is now online with **${commandCount}** modules and **${eventCount}** events loaded.`, LogType.INFO);
@@ -22,5 +35,5 @@ export function execute(client: any) {
     botLog(`Node.js Version: ${process.version}`, LogType.INFO);
     botLog(`discord.js Version: ${version}`, LogType.INFO);
     
-    console.log(`Logged in as ${client.user.tag} (${commandCount} commands loaded)`);
+    console.log(`Logged in as ${client.user!.tag} (${commandCount} commands loaded)`);
 }
